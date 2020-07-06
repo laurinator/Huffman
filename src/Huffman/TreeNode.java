20674b900f052bb1616a0d8d51f3f1828f53ca5e
Laurin <laurin.mayer@web.de>
2020-07-06 19:09:04 +0200

    Finished TreeNode

--- src/Huffman/TreeNode.java
+++ src/Huffman/TreeNode.java
@@ -3,12 +3,16 @@ package Huffman;
 public class TreeNode {
 
     public int value;
+    public  boolean isLeaf;
     public TreeNode left;
     public TreeNode right;
 
     public TreeNode(int value){
 
         this.value = value;
+        left = null;
+        right = null;
+        isLeaf = true;
 
     }
 
@@ -17,7 +21,31 @@ public class TreeNode {
         this.value = value;
         this.left = left;
         this.right = right;
+        isLeaf = false;
 
     }
 
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
 }
