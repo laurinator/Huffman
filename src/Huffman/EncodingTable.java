c00301144cbe0dcff29a06763fcc87d80d5fb5b8
Laurin <laurin.mayer@web.de>
2020-07-09 13:55:25 +0200

    Created EncodingTable

---
+++ src/Huffman/EncodingTable.java
@@ -0,0 +1,24 @@
+package Huffman;
+
+import java.util.ArrayList;
+
+public class EncodingTable {
+
+    private ArrayList<Entry> entries = new ArrayList<Entry>();
+
+    public void builFromTree(TreeNode tree) {
+        //TODO: implement
+    }
+
+    private class Entry{
+        public char character;
+        public byte[] code;
+
+        public Entry(char character, byte[] code){
+            this.character = character;
+            this.code = code;
+        }
+    }
+
+
+}
